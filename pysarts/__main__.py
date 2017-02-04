488e51e00b281fe3e96f380f774c1184f0431263
Alex Jackson <alex@alexj.org>
2017-02-04 09:38:05 +0000

    Re-implement main module argument parsing with argparse
    This makes each stage of the workflow independently executable.

--- pysarts/__main__.py
+++ pysarts/__main__.py
@@ -1,15 +1,39 @@
+import argparse
 import sys
 import os
+
 from . import workflow
 
-# Parse Arguments
-# TODO: Rewrite using argparse
-if len(sys.argv) != 2:
-    print("USAGE: pysarts PROJECT_DIR")
+def execute_all_steps():
+    workflow.execute_load_clip_resample_convert_step()
+    workflow.execute_invert_unwrapped_phase()
+    workflow.execute_calculate_master_atmosphere()
 
-PROJECT_DIR = sys.argv[1]
-os.chdir(PROJECT_DIR)
-workflow.load_config('config.yml')
-workflow.execute_load_clip_resample_convert_step()
-workflow.execute_invert_unwrapped_phase()
-workflow.execute_calculate_master_atmosphere()
+mainParser = argparse.ArgumentParser(prog='pysarts')
+mainParser.set_defaults(func=execute_all_steps)
+mainParser.add_argument('-d', '--directory',
+                        action='store',
+                        default='.',
+                        help='Path to the project directory')
+mainParser.add_argument('-c', '--config',
+                        action='store',
+                        default='config.yml',
+                        help='Path to a project configuration file')
+
+subparsers = mainParser.add_subparsers()
+clip_resample_parser = subparsers.add_parser('clip-resample',
+                                             help='Clip and resample unwrapped interferograms')
+clip_resample_parser.set_defaults(func=workflow.execute_load_clip_resample_convert_step)
+invert_unwrapped_parser = subparsers.add_parser('invert',
+                                               help='Invert unwrapped time series interferograms')
+invert_unwrapped_parser.set_defaults(func=workflow.execute_invert_unwrapped_phase)
+calculate_master_atmos_parser = subparsers.add_parser('master-atmos',
+                                                      help='Calculate the master atmosphere')
+calculate_master_atmos_parser.set_defaults(func=workflow.execute_calculate_master_atmosphere)
+
+# Parse Arguments
+args = mainParser.parse_args()
+os.chdir(args.directory)
+workflow.load_config(args.config)
+
+args.func()
