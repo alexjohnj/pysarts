c1eb31049b3d9538bd6299b2999dd2d19bba852d
Alex Jackson <alex@alexj.org>
2017-02-04 14:57:52 +0000

    Add sub-command to run inversion on all SLC dates

--- pysarts/__main__.py
+++ pysarts/__main__.py
@@ -1,14 +1,41 @@
-from datetime import datetime
+"""pysarts interactive usage module.
+"""
 import argparse
-import sys
+from datetime import datetime
+import logging
 import os
 
 from . import workflow
+from . import util
 
 def execute_all_steps():
     workflow.execute_load_clip_resample_convert_step()
     workflow.execute_invert_unwrapped_phase()
     workflow.execute_calculate_master_atmosphere()
+
+def execute_on_all_slc_dates():
+    """Executes the full processing stack on all the SLC dates in the UIFG_DIR
+    directory that aren't filtered by the config file.
+
+    """
+    # First find all the dates in the UIFG_DIR directory
+    ifg_paths = workflow.find_ifgs()
+    ifg_date_pairs = [util.extract_timestamp_from_ifg_name(path) for path in ifg_paths]
+    ifg_dates = set([date for pair in ifg_date_pairs for date in pair])
+
+    # Copy the acquisition
+    slc_time = workflow.config.MASTER_DATE.time()
+
+    # Only need to run the clip, resample, convert step once.
+    workflow.execute_load_clip_resample_convert_step()
+
+    # Let's go!
+    logging.info('Going to run processing workflow on %d master dates', len(ifg_dates))
+    for date in ifg_dates:
+        logging.info('Switching master date to %s', date.strftime('%Y-%m-%d'))
+        workflow.config.MASTER_DATE = datetime.combine(date, slc_time)
+        workflow.execute_invert_unwrapped_phase()
+        workflow.execute_calculate_master_atmosphere()
 
 mainParser = argparse.ArgumentParser(prog='pysarts')
 mainParser.set_defaults(func=execute_all_steps)
@@ -38,6 +65,12 @@ calculate_master_atmos_parser.set_defaults(func=workflow.execute_calculate_maste
 weather_radar_parser = subparsers.add_parser('radar-correlation',
                                              help='Calculate the correlation between weather radar rainfall and master atmosphere.')
 weather_radar_parser.set_defaults(func=workflow.execute_master_atmosphere_rainfall_correlation)
+
+god_mode_parser = subparsers.add_parser('alldates',
+                                        help="""Run the full processing workflow
+                                        on all dates allowed in the
+                                        configuration file.""")
+god_mode_parser.set_defaults(func=execute_on_all_slc_dates)
 
 # Parse Arguments
 args = mainParser.parse_args()
