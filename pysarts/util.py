7cd417608e1f287d609a7e63ccfe22504058a57a
Alex Jackson <alex@alexj.org>
2017-03-20 09:43:13 +0000

    Add JIT compiled linear interpolation function

--- pysarts/util.py
+++ pysarts/util.py
@@ -2,6 +2,8 @@ import os.path
 from datetime import datetime
 
 from netCDF4 import Dataset
+from numba import jit
+import numpy as np
 
 def extract_timestamp_from_ifg_name(file_name):
     """Extract master and slave date objects from the name of an interferogram.
@@ -39,3 +41,63 @@ def load_dem(path):
         dem['data'] = df.variables['Band1'][:, :]
 
         return dem
+
+
+# Taken from David-OConnor/brisk.
+@jit(nopython=True)
+def bisect(a, x):
+    """Similar to bisect.bisect() from the built-in library."""
+    M = a.size
+    for i in range(M):
+        if a[i] > x:
+            return i
+
+    return M
+
+
+@jit
+def interp1d(xs, ys, xis):
+    """1D linear interpolation of points with linear extrapolation.
+
+    Arguments
+    ---------
+    xs : (n,) ndarray
+      Points that `ys` have been calculated at.
+    ys : (n,) ndarray
+      Value of function at `xs`.
+    xis : (m,) ndarray
+      Points to interpolate at.
+
+    Returns
+    -------
+    (m,) ndarray containing values interpolated from `ys` at points
+    `xis`. Points in `xis` falling outside of `xs` will be extrapolated using a
+    linear extrapolation scheme.
+
+    Notes
+    -----
+    This implementation is based on the linear interpolation implemented in
+    David-OConnor/brisk. The scheme has been extended to include linear
+    extrapolation for points outside of the input points however.
+    """
+    yis = np.empty(xis.size, dtype=np.float)
+    interp1d_jit(xs, ys, xis, yis)
+    return yis
+
+
+@jit(nopython=True)
+def interp1d_jit(xs, ys, xis, yis):
+    for idx in range(xis.size):
+        idx1 = bisect(xs, xis[idx])
+        idx0 = idx1 - 1
+
+        # Extrapolate if interpolation point falls outside of xs.
+        if idx1 == 0:
+            idx1 = 1
+            idx0 = 0
+        elif idx1 == xs.size:
+            idx1 = -1
+            idx0 = -2
+
+        yis[idx] = ys[idx0] + ((xis[idx] - xs[idx0]) * (ys[idx1] - ys[idx0])
+                               / (xs[idx1] - xs[idx0]))
